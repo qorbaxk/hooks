5dba078abf222afde035da95022ecfa22e7848ba
Butter on Bread <qorbaxk97@gmail.com>
2023-02-22 20:54:36 +0900

    useConfirm, usePreventLeave 학습

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,3 +3,5 @@ export { default as UseInput } from "./useInput/UseInput";
 export { default as UseTitle } from "./useTitle/UseTitle";
 export { default as UseClick } from "./useClick/UseClick";
 export { default as UseHover } from "./useHover/UseHover";
+export { default as UseConfirm } from "./useConfirm/UseConfirm";
+export { default as UsePreventLeave } from "./usePreventLeave/UsePreventLeave";
---
+++ src/hooks/useConfirm/UseConfirm.js
@@ -0,0 +1,18 @@
+const UseConfirm = (message="", onConfirm, onCancel)=>{
+  if(typeof onConfirm !== "function"){
+    return;
+  }
+  if(typeof onCancel !== "function"){
+    return;
+  }
+  const confirmAction = () =>{
+    if(window.confirm(message)){
+      onConfirm();
+    }else{
+      onCancel();
+    }
+  };
+  return confirmAction;
+}
+
+export default UseConfirm;
