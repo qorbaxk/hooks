85c477d54136e78b0e5637728dd8857b01c07b2d
Butter on Bread <qorbaxk97@gmail.com>
2023-02-22 22:37:50 +0900

    useNetwork 학습

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import "./App.css";
-import { UseFadeIn } from "./hooks";
+import { UseNetwork } from "./hooks";
 
 const App = () => {
-  const fadeInH1 = UseFadeIn(1, 2);
-  const fadeInP = UseFadeIn(5, 6);
+  const handleNetworkChange = (online) => {
+    console.log(online ? "i'm online" : "i'm offline");
+  };
+
+  const onLine = UseNetwork(handleNetworkChange);
 
   return (
     <>
-      <h1 {...fadeInH1}>hi!</h1>
-      <p {...fadeInP}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
-        voluptatem, assumenda explicabo dignissimos ea veniam facere voluptatum
-        eaque inventore optio autem amet id repudiandae qui, in rem sed
-        perferendis sunt!
-      </p>
+      <h1>{onLine ? "Online" : "Offline"}</h1>
     </>
   );
 };
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -7,4 +7,5 @@ export { default as UseConfirm } from "./useConfirm/UseConfirm";
 export { default as UsePreventLeave } from "./usePreventLeave/UsePreventLeave";
 export { default as UseBeforeLeave } from "./useBeforeLeave/UseBeforeLeave";
 export { default as UseFadeIn } from "./useFadeIn/UseFadeIn";
+export { default as UseNetwork } from "./useNetwork/UseNetwork";
 
