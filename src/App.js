c5d4fcb2a63e8d53ba13a50be50ccab2767e1a1a
Butter on Bread <qorbaxk97@gmail.com>
2023-02-22 16:33:50 +0900

    파일 구조 조정 및 useEffect 학습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  return <>hooks 배우는 중!</>;
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [anumber, setAnumber] = useState(0);
+  useEffect(() => {
+    sayHello();
+  }, []);
+  return (
+    <>
+      <h1>hi!</h1>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(anumber + 1)}>{anumber}</button>
+    </>
+  );
 };
 
 export default App;
---
+++ src/hooks/index.js
@@ -0,0 +1,2 @@
+export { default as UseTabs } from "./useTabs/UseTabs";
+export { default as UseInput } from "./useInput/UseInput";
